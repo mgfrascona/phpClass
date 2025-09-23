811841f34bae9f4c7813ed8da0426d3cbd746365
Michael Frascona <michaelgfrascona@gmail.com>
2025-09-22 19:12:08 -0500

    finished 8 ball

--- includes/navigation.php
+++ includes/navigation.php
@@ -4,6 +4,7 @@
 $isHome = $_SERVER['REQUEST_URI'] == '/';
 $isLoops = $isHome = $_SERVER['REQUEST_URI'] == '/loops/';
 $isCountdown = $isHome = $_SERVER['REQUEST_URI'] == '/countdown/';
+$isMagic8Ball = $isHome = $_SERVER['REQUEST_URI'] == '/magic-8ball/';
 
 ?><nav>
     <?=$_SERVER['REQUEST_URI']?>
@@ -12,5 +13,6 @@ $isCountdown = $isHome = $_SERVER['REQUEST_URI'] == '/countdown/';
         <li class = "<?=$isHome?>"><a href="/">Home</a></li>
         <li class = "<?=$isLoops?>"><a href="/loops">Loops</a></li>
         <li class = "<?=$isCountdown?>"><a href="/countdown">Countdown</a></li>
+        <li class = "<?=$isMagic8Ball?>"><a href="/magic-8ball">Magic 8 Ball</a></li>
     </ul>
 </nav>
---
+++ tests/receive.php
@@ -0,0 +1,13 @@
+<?php
+    if (isset($POST['firstName'])) echo "got a fn";
+    if (isset($POST['lastName'])) echo "got a ln";
+?>
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Showing Data</title>
+    </head>
+    <body>
+
+    </body>
+    </html><?php
